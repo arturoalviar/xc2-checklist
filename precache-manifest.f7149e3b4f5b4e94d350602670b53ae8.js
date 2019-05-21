d911d626a832a8f9a7fb5e53f5dfc2faf3cfc88a
arturoalviar <arturoalviar@gmail.com>
2019-05-21 18:54:38 -0400

    Updates

---
+++ precache-manifest.f7149e3b4f5b4e94d350602670b53ae8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c44b5353da23b9e586",
+    "url": "/xc2-checklist/static/css/main.b019885b.chunk.css"
+  },
+  {
+    "revision": "c3c44b5353da23b9e586",
+    "url": "/xc2-checklist/static/js/main.c3c44b53.chunk.js"
+  },
+  {
+    "revision": "438e0a1ad3ebdb404ca4",
+    "url": "/xc2-checklist/static/css/1.d64bac99.chunk.css"
+  },
+  {
+    "revision": "438e0a1ad3ebdb404ca4",
+    "url": "/xc2-checklist/static/js/1.438e0a1a.chunk.js"
+  },
+  {
+    "revision": "b703e51612bbeb5e2656",
+    "url": "/xc2-checklist/static/js/runtime~main.b703e516.js"
+  },
+  {
+    "revision": "ee5b1bee959a95bd43b223ec901d098a",
+    "url": "/xc2-checklist/static/media/octicons.ee5b1bee.woff"
+  },
+  {
+    "revision": "de59a97248b44599e6747a27a943f738",
+    "url": "/xc2-checklist/static/media/octicons.de59a972.woff2"
+  },
+  {
+    "revision": "e0d4a324833e13be7d4fa762146d0a71",
+    "url": "/xc2-checklist/static/media/octicons.e0d4a324.ttf"
+  },
+  {
+    "revision": "d038ccbc4a99be24f33a54b482b2422e",
+    "url": "/xc2-checklist/static/media/octicons.d038ccbc.eot"
+  },
+  {
+    "revision": "190e72572ee84190f4777765fa49fc47",
+    "url": "/xc2-checklist/static/media/octicons.190e7257.svg"
+  },
+  {
+    "revision": "475621b9a5279b19b52561b83187dfb3",
+    "url": "/xc2-checklist/index.html"
+  }
+];
